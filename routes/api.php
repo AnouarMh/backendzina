377682fd01dbf200b78b7fa7b9989e4ef9db8940
Mhamdi Anouar <127235412+AnouarMh@users.noreply.github.com>
2023-08-07 12:44:20 +0100

    Add files via upload

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SuperadminController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,10 +22,22 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::prefix('superadmin')->group(function () {
     Route::post('login', [SuperadminController::class, 'login']);
-    Route::middleware('auth:sanctum')->group(function () {
+    Route::middleware('auth:superadmin')->group(function () {
         Route::post('logout', [SuperadminController::class, 'logout']);
         Route::get('admins', [SuperadminController::class, 'getAdmins']);
         Route::get('fournisseurs', [SuperadminController::class, 'getFournisseurs']);
         Route::get('clients/{adminId}', [SuperadminController::class, 'getClientByAdmin']);
+    });
+});
+
+Route::prefix('admin')->group(function () {
+    Route::post('create', [AdminController::class, 'createCenter']);
+    Route::post('login', [AdminController::class, 'login']);
+
+    Route::middleware('auth:admin')->group(function () {
+        Route::get('manage-clients', [AdminController::class, 'manageClients']);
+        Route::get('manage-reservations', [AdminController::class, 'manageReservations']);
+        Route::get('manage-services', [AdminController::class, 'manageServices']);
+        Route::post('order-products', [AdminController::class, 'orderProductsFromFournisseur']);
     });
 });
