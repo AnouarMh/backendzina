7f002961c8c83ba389c1e977149059b3bb43bdc5
Mhamdi Anouar <127235412+AnouarMh@users.noreply.github.com>
2023-08-07 15:05:04 +0100

    Add files via upload

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -14,6 +14,7 @@ use App\Models\Commande;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Facades\Session;
+
 class AdminController extends Controller
 {
     public function createCenter(Request $request)
@@ -34,7 +35,7 @@ class AdminController extends Controller
         // ... Autres étapes de validation et de traitement
         //donne la valeur  de superadmin_id si la superadmin ajouter le centre si nn donne 0
         $data['superadmin_id'] = Auth::guard('superadmin')->user()->id;
-        
+
     
         $data['password'] = Hash::make($data['password']);
         $admin = Admin::create($data);
@@ -45,26 +46,59 @@ class AdminController extends Controller
 
     public function login(Request $request)
     {
-        // Logique pour gérer la connexion d'un centre (admin)
+        $credentials = $request->only('email', 'password');
+        $admin = Admin::where('email', $credentials['email'])->first();
+    
+        if (!$admin || !Hash::check($credentials['password'], $admin->password)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+    
+        $token = $admin->createToken('admin-token')->plainTextToken;
+        return response()->json(['token' => $token], 200);
     }
 
     public function manageClients()
     {
         // Logique pour gérer les clients du centre
+
+        $clients = Client::all();
+        return view('admin.manageClients', compact('clients'));
+
     }
 
     public function manageReservations()
     {
         // Logique pour gérer les réservations du centre
+        $reservations = Reservation::all();
+        return view('admin.manageReservations', compact('reservations'));
     }
 
     public function manageServices()
     {
+
         // Logique pour gérer les services du centre
+        $services = Service::all();
+        return view('admin.manageServices', compact('services'));
+
     }
 
     public function orderProductsFromFournisseur(Request $request)
     {
         // Logique pour commander des produits auprès des fournisseurs
+        $request->validate([
+            'produit_id' => 'required',
+            'quantite' => 'required',
+            'fournisseur_id' => 'required',
+        ]);
+
+        $data = $request->only([
+            'produit_id', 'quantite', 'fournisseur_id'
+        ]);
+
+        $data['admin_id'] = Auth::guard('admin')->user()->id;
+
+        $commande = Commande::create($data);
+
+        return response()->json(['message' => 'Commande créée avec succès', 'commande' => $commande], 201);
     }
 }
