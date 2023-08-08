ceec68bfb379b5ed8ce4297cb7097379dcc8295f
Mhamdi Anouar <127235412+AnouarMh@users.noreply.github.com>
2023-08-08 15:27:24 +0100

    Add files via upload

--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,14 @@ Route::prefix('superadmin')->group(function () {
     Route::post('login', [SuperadminController::class, 'login']);
     Route::middleware('auth:superadmin')->group(function () {
         Route::post('logout', [SuperadminController::class, 'logout']);
+         Route::post('update/{admin}', [SuperadminController::class, 'updateSuperadmin']);
+         Route::post('restemail', [SuperadminController::class, 'resetEmail']);
+
+         Route::post('resetpwd', [SuperadminController::class, 'resetPassword']);
+         Route::post('logout', [SuperadminController::class, 'logout']);
+
         Route::get('admins', [SuperadminController::class, 'getAdmins']);
+        Route::get('clients/{centerId}', [SuperadminController::class, 'getClientByCenter']);
         Route::get('fournisseurs', [SuperadminController::class, 'getFournisseurs']);
         Route::get('clients/{adminId}', [SuperadminController::class, 'getClientByAdmin']);
     });
@@ -35,7 +42,8 @@ Route::prefix('admin')->group(function () {
     Route::post('login', [AdminController::class, 'login']);
 
     Route::middleware('auth:admin')->group(function () {
-        Route::get('manage-clients', [AdminController::class, 'manageClients']);
+        Route::post('createClient', [AdminController::class, 'createClient']);
+        Route::get('manage-clients', [AdminController::class, 'getClients']);
         Route::get('manage-reservations', [AdminController::class, 'manageReservations']);
         Route::get('manage-services', [AdminController::class, 'manageServices']);
         Route::post('order-products', [AdminController::class, 'orderProductsFromFournisseur']);
